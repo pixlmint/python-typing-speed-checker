bd12b32eb890f61915bf3e7354b277f49bc3eb12
pixlmint <swiss8oy.chg@gmail.com>
2023-10-07 21:25:49 +0200

    unittest -> pytest

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
-import unittest
 import main
 
 
-class TestType(unittest.TestCase):
-    def test_get_poem_lines(self):
-        lines = main.get_poem_lines(2)
-        self.assertIsInstance(lines, list)
-        self.assertIs(2, len(lines))
+def test_get_poem_lines():
+    lines = main.get_poem_lines(2)
+    assert len(lines) == 2
+    assert isinstance(lines, list)
